c5bd370de01af9b992d7e443555fcb52cec75ed2
screendragons <chiyuyeung@outlook.com>
2020-02-20 15:03:57 +0100

    After #26

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -7,14 +7,28 @@ class ChooseLocation extends StatefulWidget {
 
 class _ChooseLocationState extends State<ChooseLocation> {
 
-  int counter = 0;
+  void getData() async {
+    // simulate network request for username
+    String username = await Future.delayed(Duration(seconds: 3), () {
+      return 'yoshi';
+    });
+
+    // simulate network request to get bio of the username
+    String bio = await Future.delayed(Duration(seconds: 2), () {
+      return 'vegan, musician & egg collector';
+    });
+
+    print('$username - $bio');
+  }
 
   @override
   void initState() {
     super.initState();
-    print('initState function ran');
+//    print('initState function ran');
+      getData();
+    print('hey there!');
   }
-  
+
   @override
   Widget build(BuildContext context) {
     print('build function ran');
@@ -25,14 +39,6 @@ class _ChooseLocationState extends State<ChooseLocation> {
         title: Text('Choose a location'),
         centerTitle: true,
         elevation: 0,
-      ),
-      body: RaisedButton(
-        onPressed: () {
-          setState(() {
-            counter += 1 ;
-          });
-        },
-        child: Text('counter is $counter'),
       ),
     );
   }
