ee7051595c55e6439a5ba800686ecfdadbb32dac
screendragons <chiyuyeung@outlook.com>
2020-02-20 16:06:57 +0100

    After #26 and #27

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'dart:convert';
 
 class Loading extends StatefulWidget {
   @override
@@ -6,6 +8,32 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
+
+  void getTime() async {
+    // make the request
+    Response response = await get('http://worldtimeapi.org/api/timezone/Europe/London');
+    Map data = jsonDecode(response.body);
+    // print(data);
+
+    // get properties from data
+    String datetime = data['datetime'];
+    String offset = data['utc_offset'].substring(1,3);
+    // print(datetime);
+    // print(offset);
+
+    // create DateTime object
+    DateTime now = DateTime.parse(datetime);
+    now = now.add(Duration(hours: int.parse(offset)));
+    print(now);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+//    print('initState function ran');
+    getTime();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
